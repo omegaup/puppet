a0b1bcfc0eb4c53c0976ae8e8e5869a870cab66f
lhchavez <lhchavez@lhchavez.com>
2018-12-30 16:50:26 +0000

    Install the gitserver binaries
    This installs /usr/bin/omegaup-{migrate,update}-problem.

--- manifests/services.pp
+++ manifests/services.pp
@@ -32,6 +32,46 @@ class omegaup::services {
           '/usr/bin/omegaup-broadcaster']:
     require => Exec['omegaup-backend'],
   }
+
+  remote_file { '/usr/bin/omegaup-migrate-problem.xz':
+    url      => 'https://github.com/omegaup/gitserver/releases/download/v1.2.0/omegaup-migrate-problem.xz',
+    sha1hash => '3880e4dae6790d16bc9a0624fd501b48c4f21a2c',
+    mode     => '644',
+    owner    => 'root',
+    group    => 'root',
+    notify   => Exec['omegaup-migrate-problem'],
+  }
+
+  exec { 'omegaup-migrate-problem':
+    command     => '/usr/bin/unxz --force --keep /usr/bin/omegaup-migrate-problem.xz',
+    user        => 'root',
+    notify      => File['/usr/bin/omegaup-migrate-problem'],
+    refreshonly => true,
+  }
+
+  file { '/usr/bin/omegaup-migrate-problem':
+    require => Exec['omegaup-migrate-problem'],
+  }
+
+  remote_file { '/usr/bin/omegaup-update-problem.xz':
+    url      => 'https://github.com/omegaup/gitserver/releases/download/v1.2.0/omegaup-update-problem.xz',
+    sha1hash => 'fd98f3c1f66de2b4dee2af4065a526a0aa88a508',
+    mode     => '644',
+    owner    => 'root',
+    group    => 'root',
+    notify   => Exec['omegaup-update-problem'],
+  }
+
+  exec { 'omegaup-update-problem':
+    command     => '/usr/bin/unxz --force --keep /usr/bin/omegaup-update-problem.xz',
+    user        => 'root',
+    notify      => File['/usr/bin/omegaup-update-problem'],
+    refreshonly => true,
+  }
+
+  file { '/usr/bin/omegaup-update-problem':
+    require => Exec['omegaup-update-problem'],
+  }
 }
 
 # vim:expandtab ts=2 sw=2
