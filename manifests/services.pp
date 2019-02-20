fc662eb9bcc64fb12fe8bbc849cec5540129d98a
lhchavez <lhchavez@lhchavez.com>
2019-02-20 03:07:13 +0000

    Backend refresh

--- manifests/services.pp
+++ manifests/services.pp
@@ -1,8 +1,8 @@
 # The omegaUp services.
 class omegaup::services {
   remote_file { '/var/lib/omegaup/omegaup-backend.tar.xz':
-    url      => 'https://github.com/omegaup/quark/releases/download/v1.1.8/omegaup-backend.tar.xz',
-    sha1hash => '4c7a235be73622390b8993d5f51ac076718d7839',
+    url      => 'https://github.com/omegaup/quark/releases/download/v1.1.9/omegaup-backend.tar.xz',
+    sha1hash => 'b18d202c2dc6b87304ffba3117e07f64a43371bb',
     mode     => '644',
     owner    => 'root',
     group    => 'root',
