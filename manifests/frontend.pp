02d92925e05227b836549ad4e3a7329c770760d6
lhchavez <lhchavez@lhchavez.com>
2019-04-04 02:36:50 +0000

    Stop using the deprecated hiera_lookup().

--- manifests/frontend.pp
+++ manifests/frontend.pp
@@ -1,4 +1,4 @@
-hiera_include('classes')
+lookup('classes', {merge => unique}).include
 
 host { 'localhost':
   ensure => present,
--- manifests/frontend_org.pp
+++ manifests/frontend_org.pp
@@ -1,4 +1,4 @@
-hiera_include('classes')
+lookup('classes', {merge => unique}).include
 
 # A few entries to avoid taking a dependency on ::omegaup.
 package { 'sudo':
