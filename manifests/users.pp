a4d7f79fd5bdb6b7fce68500603eddd63466e4bb
lhchavez <lhchavez@lhchavez.com>
2020-10-11 04:57:23 -0700

    Create the omegaup-www home directory
    This is required for omegaup-uprev

--- manifests/users.pp
+++ manifests/users.pp
@@ -1,6 +1,10 @@
 # Creates users needed at runtime.
 class omegaup::users {
-  user { ['omegaup', 'www-data', 'omegaup-www', 'omegaup-deploy']: ensure => present }
+  user { ['omegaup', 'www-data', 'omegaup-deploy']: ensure => present }
+  user { 'omegaup-www':
+    ensure => present,
+    managehome => true,
+  }
 }
 
 # vim:expandtab ts=2 sw=2
